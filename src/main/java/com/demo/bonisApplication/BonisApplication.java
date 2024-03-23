03e56d2e758e6d92586ad11ca141875355ccd958
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-03-23 12:36:02 -0300

    Project Created

---
+++ src/main/java/com/demo/bonisApplication/BonisApplication.java
@@ -0,0 +1,13 @@
+package com.demo.bonisApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/bonisApplication/BonisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.bonisApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
