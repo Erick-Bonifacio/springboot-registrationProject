f408e6e9209417218c84a4478259f4d6055497b1
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-04-02 08:53:44 -0300

    Entity Payment

---
+++ src/main/java/com/demo/bonisApplication/entities/Payment.java
@@ -0,0 +1,75 @@
+package com.demo.bonisApplication.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_payment")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Double value;
+	private Date date;
+	
+	public Payment() {}
+
+	public Payment(Long id, Double value, Date date) {
+		this.id = id;
+		this.value = value;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
